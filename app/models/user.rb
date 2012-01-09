d08b9fcc9330d4dbd39fb9704cc03ea1cbbf92a0
Junichi Ito <june.1st.717@gmail.com>
2012-01-09 17:14:55 +0900

    by List 11.32

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,36 +1,33 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#
 require 'digest'
 class User < ActiveRecord::Base
-    attr_accessor :password
-    attr_accessible :name, :email, :password, :password_confirmation
-    email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :name, :presence => true, :length => { :maximum => 50 }
-    validates :email, :presence => true, :format => { :with => email_regex }, :uniqueness => { :case_sensitive => false } 
-    validates :password, :presence => true, :confirmation => true, :length => { :within => 6..40 }
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
+  has_many :microposts, :dependent => :destroy
 
-    before_save :encrypt_password
-    def has_password?(submitted_password)
-        encrypted_password == encrypt(submitted_password)
-    end
-    def self.authenticate(email, submitted_password)
-        user = find_by_email(email)
-        return nil if user.nil?
-        return user if user.has_password?(submitted_password)
-    end
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, :presence => true, :length => { :maximum => 50 }
+  validates :email, :presence => true, :format => { :with => email_regex }, :uniqueness => { :case_sensitive => false } 
+  validates :password, :presence => true, :confirmation => true, :length => { :within => 6..40 }
 
-    def self.authenticate_with_salt(id, cookie_salt)
-      user = find_by_id(id)
-      (user && user.salt == cookie_salt) ? user : nil
-    end
+  before_save :encrypt_password
+  def has_password?(submitted_password)
+      encrypted_password == encrypt(submitted_password)
+  end
+  def self.authenticate(email, submitted_password)
+      user = find_by_email(email)
+      return nil if user.nil?
+      return user if user.has_password?(submitted_password)
+  end
+
+  def self.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    (user && user.salt == cookie_salt) ? user : nil
+  end
+
+  def feed
+    # This is preliminary. See Chapter 12 for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
     private
         def encrypt_password
             self.salt = make_salt unless has_password?(password)
